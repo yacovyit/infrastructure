9c220945ba18c7d65fde00a9b2a0a417ba4aac8a
yacovyit <yacov.mobile@gmail.com>
2018-06-25 23:51:45 +0300

    Defaulting to no-operation (NOP) logger implementation

--- kafka/src/main/java/BasicProducerExample.java
+++ kafka/src/main/java/BasicProducerExample.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.Random;
 
 public class BasicProducerExample {
+
+    public static void main(String[] args){
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        TestCallback callback = new TestCallback();
+        Random rnd = new Random();
+        for (long i = 0; i < 100 ; i++) {
+            ProducerRecord<String, String> data = new ProducerRecord<String, String>(
+                    "test-topic", "key-" + i, "message-"+i );
+            producer.send(data, callback);
+        }
+
+        producer.close();
+    }
+
+
+    private static class TestCallback implements Callback {
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            if (e != null) {
+                System.out.println("Error while producing message to topic :" + recordMetadata);
+                e.printStackTrace();
+            } else {
+                String message = String.format("sent message to topic:%s partition:%s  offset:%s", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
+                System.out.println(message);
+            }
+        }
+    }
+
 }
