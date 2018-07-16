916a49c0da8abbe6150b35fe035178ee003e6324
yacovyit <yacov.mobile@gmail.com>
2018-07-16 18:13:33 +0300

    add elasticsearch data-init script

---
+++ elasticsearch/src/main/resources/data-init/data-init.sh
@@ -0,0 +1,74 @@
+unzip accounts.zip & gunzip logs.jsonl.gz
+curl -X PUT "localhost:9200/shakespeare" -H 'Content-Type: application/json' -d'
+{
+ "mappings": {
+  "doc": {
+   "properties": {
+    "speaker": {"type": "keyword"},
+    "play_name": {"type": "keyword"},
+    "line_id": {"type": "integer"},
+    "speech_number": {"type": "integer"}
+   }
+  }
+ }
+}
+'
+curl -X PUT "localhost:9200/logstash-2015.05.18" -H 'Content-Type: application/json' -d'
+{
+  "mappings": {
+    "log": {
+      "properties": {
+        "geo": {
+          "properties": {
+            "coordinates": {
+              "type": "geo_point"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'
+curl -X PUT "localhost:9200/logstash-2015.05.19" -H 'Content-Type: application/json' -d'
+{
+  "mappings": {
+    "log": {
+      "properties": {
+        "geo": {
+          "properties": {
+            "coordinates": {
+              "type": "geo_point"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'
+
+
+curl -X PUT "localhost:9200/logstash-2015.05.20" -H 'Content-Type: application/json' -d'
+{
+  "mappings": {
+    "log": {
+      "properties": {
+        "geo": {
+          "properties": {
+            "coordinates": {
+              "type": "geo_point"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'
+
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/bank/account/_bulk?pretty' --data-binary @accounts.json
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/shakespeare/doc/_bulk?pretty' --data-binary @shakespeare_6.0.json
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/_bulk?pretty' --data-binary @logs.jsonl
+
+
